1235a1d02f47b01c9fbae9b43fc149e0b6b3f300
Eastabba <188729351+Eastabba@users.noreply.github.com>
2025-12-22 23:56:56 +0800

    没搞好

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js - 存储页面预设值
+
+const jsonData = {
+  // “具体时间”
+  operation_time: {
+    hour: "14",
+    minute: "10"
+  },
+
+  // “班主任批准时间”预设的时分秒
+  approval_time: "14:04:39",
+
+  // “操作时间”
+  czsj: "14:01:44" // 示例：只设置时分秒
+};
