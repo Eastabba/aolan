54429db5b4a40a3e2bec80c69843bc8cda278bfa
Eastabba <188729351+Eastabba@users.noreply.github.com>
2025-12-23 12:04:57 +0800

    time updated

--- data.js
+++ data.js
@@ -4,12 +4,12 @@ const jsonData = {
   // “具体时间”
   operation_time: {
     hour: "14",
-    minute: "10"
+    minute: "15"
   },
 
   // “班主任批准时间”
-  approval_time: "14:02:39",
+  approval_time: "14:05:39",
 
   // “操作时间”
-  czsj: "14:01:44"
+  czsj: "14:05:44"
 };
