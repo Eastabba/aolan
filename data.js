7ff34109febc00e2896d963d215596dc038207a4
Eastabba <188729351+Eastabba@users.noreply.github.com>
2025-12-23 11:47:39 +0800

    test

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js - 存储页面预设值
+
+const jsonData = {
+  // “具体时间”
+  operation_time: {
+    hour: "14",
+    minute: "10"
+  },
+
+  // “班主任批准时间”
+  approval_time: "14:03:39",
+
+  // “操作时间”
+  czsj: "14:01:44"
+};
