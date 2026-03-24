3fc71b0db9628b8579397ea7430e93cba7c52ff0
Eastabba <188729351+Eastabba@users.noreply.github.com>
2026-03-24 13:15:47 +0800

    Update operation and approval times in data.js

--- data.js
+++ data.js
@@ -3,13 +3,13 @@
 const jsonData = {
   // “具体时间”
   operation_time: {
-    hour: "12",
-    minute: "00"
+    hour: "14",
+    minute: "10"
   },
 
   // “班主任批准时间”
-  approval_time: "12:05:39",
+  approval_time: "14:15:39",
 
   // “操作时间”
-  czsj: "12:00:27"
+  czsj: "14:10:27"
 };
